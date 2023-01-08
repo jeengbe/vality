a651881158d586a975b0f5c159524518e482f6f8
Jesper Engberg <jeengbe@gmail.com>
2023-01-08 10:02:15 +0100

    feat(vality): export more

--- packages/vality/src/index.ts
+++ packages/vality/src/index.ts
@@ -1,12 +1,17 @@
 import "./config";
 
-// Import inbuilt guards and valits
+// Import inbuilt Guards and Valits
 import "./compounds";
 import "./flags";
 import "./scalars";
 
-// Export vality
+// Export Vality
 export * from "./parse";
-export { extendScalar, Scalar, ScalarFn } from "./scalar";
 export * from "./validate";
 export * from "./vality";
+
+// Stuff for extending Vality
+export * from "./utils";
+export * from "./typeUtils";
+export { scalar, extendScalar, Scalar, ScalarFn } from "./scalar";
+export { compound, Compound, CompoundFn } from "./compound";
